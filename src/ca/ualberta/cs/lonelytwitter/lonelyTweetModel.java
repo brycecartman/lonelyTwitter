cf79539a4ea11ddda5846541410e4aa2693ca177
brycecartman <cartman@ualberta.ca>
2014-01-14 17:46:25 -0700

    Added a few constructors to the lonelyTweetModel class.

--- src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/lonelyTweetModel.java
@@ -21,5 +21,17 @@ public class lonelyTweetModel {
 	public void setTimestamp(Date timestamp) {
 		this.timestamp = timestamp;
 	}
+
+	public lonelyTweetModel(String text, Date timestamp) {
+		super();
+		this.text = text;
+		this.timestamp = timestamp;
+	}
+	
+	public lonelyTweetModel(String text) {
+		super();
+		this.text = text;
+		this.timestamp = new Date();
+	}
 	
 }
